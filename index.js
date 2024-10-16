9667d30e1c4a54c0c5fc74e0800c56cceaa93611
leon1010000 <leoniz@outlook.jp>
2024-10-16 20:16:22 +0900

    change

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ minfact.forEach((v,i)=>{
     if(i==0||i==1||v!=i)return;
     for(let j=i*2;j<n;j+=i)if(minfact[j]==j)minfact[j]=i;
 })
-document.getElementById("enter").addEventListener("click",()=>{
-    let num=Number(document.getElementById("numinput").value);
+function onEntered(){
+    let numv=document.getElementById("numinput").value;
+    let num=Number(numv);
     let text=document.getElementById("textinput"),ret="";
-    console.log(num);
+    let com=1;
+    if(numv.length==0){
+        text.value=`Error`+(com?`: Please type a number above.`:``);
+        return;
+    }
     if(num>=n){
-        text.value=`Error:The number must be less than ${n}.`;
+        text.value=`Error`+(com?`: The number must be less than ${n}.`:``);
         return;
     }
     if(num<=1){
-        text.value=`Error:The number must be greater than or equal to 2.`;
+        text.value=`Error`+(com?`: The number must be greater than or equal to 2.`:``);
         return;
     }
     if(!Number.isInteger(num)){
-        text.value=`Error:The number must be an integer.`;
+        text.value=`Error`+(com?`: The number must be an integer.`:``);
         return;
     }
     while(num>1){
@@ -27,4 +32,8 @@ document.getElementById("enter").addEventListener("click",()=>{
         ret+=(ret.length==0?"":"*")+minf+(count>1?"^"+count:"");
     }
     text.value=ret;
+}
+document.getElementById("enter").addEventListener("click",onEntered);
+document.getElementById("numinput").addEventListener("input",ev=>{
+    onEntered();
 })
